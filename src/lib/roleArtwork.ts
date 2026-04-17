1903217829ac8c50c47de4d6e2c769ea16cee8eb
Vishnu Mohandas <vishnu@ente.io>
2026-04-17 17:04:31 +0530

    Art work

--- src/lib/roleArtwork.ts
+++ src/lib/roleArtwork.ts
@@ -1,6 +1,7 @@
 const roleArtworkByTeam: Record<string, string> = {
   engineering: "/role-art/engineering.png",
   marketing: "/role-art/marketing.png",
+  product: "/role-art/product.png",
   video: "/role-art/video.png",
 };
 
